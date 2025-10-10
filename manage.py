8a62b63ce59580e9bdba5eb6dd43db6cfb7a2ff6
AHApeN4264 <kofanandrej3@gmail.com>
2025-10-11 01:05:53 +0300

    Додана система голосування

--- manage.py
+++ manage.py
@@ -3,10 +3,9 @@
 # Для запуска с телефона
 # python manage.py runserver 0.0.0.0:8000
 
+# git checkout -b Система-голосувань
 # git add .
-# git commit -m "Added student and biary"
-# git push origin имя_ветки
-# git push origin Added student and biary
+# git push origin Система-голосувань
 
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
